a685808b3f2ae1d434b881a9ebaea11964636c22
jotapkrl67-oss <jotapkrl67@gmail.com>
2025-10-22 01:28:42 -0300

    Delete api directory

--- api/health.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).json({ 
-        status: 'OK', 
-        timestamp: new Date().toISOString() 
-    });
-}
-
